5a6f7dee8ba830422bc574bd99406ea5f60863ca
Alexandra Aulum <alex.aulum@gmail.com>
2018-07-18 22:13:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph');
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob');
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  kittens = ['Broom']
+}
